3b3331520aa6b04fe6b97735dfc432d64cd8c853
Preetam Raj <iitianpreetam@gmail.com>
2021-06-19 19:46:52 +0530

    added allowed host

--- sms_pro/settings.py
+++ sms_pro/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://pr-link-manager.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['pr-link-manager.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
